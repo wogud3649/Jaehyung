4523e4a7d39a3bd3d5d5cde9e85c27a57b493cd8
wogud3649 <wogud3649@gmail.com>
2023-04-19 17:55:30 +0900

    Unreal Engine Update

---
+++ Unreal/MyProject1/Source/MyProject1/MyProject1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyProject1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYPROJECT1_API AMyProject1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
