1c3e1a4db68e8bf7f9f6866f34fd5b6e17e0e071
wogud3649 <wogud3649@gmail.com>
2023-05-18 17:50:43 +0900

    Create LIS.cpp

---
+++ Practice/Practice/Solved/LIS.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <list>
+#include <string>
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> a = { 1,2,3,4,5,3,5,6,7,8,1,2,3 };
+vector<int> cache(a.size(), 1);
+//                        5,4,4,4,3,2,2,2,2,1
+
+void LIS(int num)
+{
+    if (num > a.size() - 2)
+        return;
+
+    LIS(num + 1);
+
+    for (int i = a.size() - 1; i > num; i--)
+    {
+        if (a[num] <= a[i])
+            cache[num] = max(cache[num], cache[i] + 1);
+    }
+
+    return;
+}
+
+int main() {
+    LIS(0);
+
+    for (int i = cache.size() - 1; i > 0; i--)
+    {
+        if (cache[i - 1] < cache[i])
+            cache[i - 1] = cache[i];
+    }
+
+    cout << *max_element(cache.begin(), cache.end()) << endl;
+
+    return 0;
+}
